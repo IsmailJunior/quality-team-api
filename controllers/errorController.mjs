f62a81f5bac07a341d70ab60271ea2db17e6f3b1
System Administrator <root@Ismails-MacBook-Air.local>
2024-09-19 02:39:21 +0300

    update

--- controllers/errorController.mjs
+++ controllers/errorController.mjs
@@ -1,30 +1,4 @@
-// import AppError from '../utils/appError.mjs';
-
-// const handleCastErrorDB = (err) => {
-// 	const message = `Invalid ${err.path}: ${err.value}.`;
-// 	return new AppError(message, 400);
-// };
-
-// const handleDuplicateFieldsErrorDB = (err) => {
-// 	const value = err.errorResponse.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-// 	const message = `Duplicate Field value: ${value}, Please use another value!`;
-// 	return new AppError(message, 400);
-// };
-
-// const handleValidationErrorDB = (err) => {
-// 	const errors = Object.values(err.errors).map((element) => element.message);
-// 	const message = `Invalid input data, ${errors.join(' ')}`;
-// 	return new AppError(message, 400);
-// };
-
-// const handleJWTError = () =>
-// 	new AppError('Invalid token, Please log in again!', 401);
-
-// const handleJWTExpireError = () =>
-// 	new AppError('Your token has expired!, Please log in again.', 401);
-
 const globalErrorController = (err, req, res, _next) => {
-	console.log(err);
 	if (err.name === 'ValidationError') {
 		// eslint-disable-next-line no-param-reassign
 		err.statusCode = 422;
