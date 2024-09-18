6e830271ee0f0e7362524c366c9caa92f5be7662
System Administrator <root@Ismails-MacBook-Air.local>
2024-09-18 17:28:02 +0300

    Refactor Errors

--- controllers/errorController.mjs
+++ controllers/errorController.mjs
@@ -1,27 +1,27 @@
-import AppError from '../utils/appError.mjs';
+// import AppError from '../utils/appError.mjs';
 
-const handleCastErrorDB = (err) => {
-	const message = `Invalid ${err.path}: ${err.value}.`;
-	return new AppError(message, 400);
-};
+// const handleCastErrorDB = (err) => {
+// 	const message = `Invalid ${err.path}: ${err.value}.`;
+// 	return new AppError(message, 400);
+// };
 
-const handleDuplicateFieldsErrorDB = (err) => {
-	const value = err.errorResponse.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-	const message = `Duplicate Field value: ${value}, Please use another value!`;
-	return new AppError(message, 400, 11000);
-};
+// const handleDuplicateFieldsErrorDB = (err) => {
+// 	const value = err.errorResponse.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+// 	const message = `Duplicate Field value: ${value}, Please use another value!`;
+// 	return new AppError(message, 400, 11000);
+// };
 
-const handleValidationErrorDB = (err) => {
-	const errors = Object.values(err.errors).map((element) => element.message);
-	const message = `Invalid input data, ${errors.join(' ')}`;
-	return new AppError(message, 400);
-};
+// const handleValidationErrorDB = (err) => {
+// 	const errors = Object.values(err.errors).map((element) => element.message);
+// 	const message = `Invalid input data, ${errors.join(' ')}`;
+// 	return new AppError(message, 400);
+// };
 
-const handleJWTError = () =>
-	new AppError('Invalid token, Please log in again!', 401);
+// const handleJWTError = () =>
+// 	new AppError('Invalid token, Please log in again!', 401);
 
-const handleJWTExpireError = () =>
-	new AppError('Your token has expired!, Please log in again.', 401);
+// const handleJWTExpireError = () =>
+// 	new AppError('Your token has expired!, Please log in again.', 401);
 
 const devError = (err, req, res) => {
 	if (err.isOperational === true) {
@@ -43,12 +43,12 @@ const devError = (err, req, res) => {
 	}
 };
 
-const prodError = (err, res) => {
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
-};
+// const prodError = (err, res) => {
+// 	res.status(err.statusCode).json({
+// 		status: err.status,
+// 		message: err.message,
+// 	});
+// };
 
 const globalErrorController = (err, req, res, _next) => {
 	// eslint-disable-next-line no-param-reassign
@@ -56,19 +56,17 @@ const globalErrorController = (err, req, res, _next) => {
 	// eslint-disable-next-line no-param-reassign
 	err.status = err.status || 'Error';
 
-	if (process.env.NODE_ENV === 'development') {
-		devError(err, req, res);
-	}
-	if (process.env.NODE_ENV === 'production') {
-		let error = { ...err };
-		if (error.name === 'CastError') error = handleCastErrorDB(error);
-		if (error.code === 11000) error = handleDuplicateFieldsErrorDB(error);
-		if (error.name === 'ValidationError')
-			error = handleValidationErrorDB(error);
-		if (error.name === 'JsonWebTokenError') error = handleJWTError();
-		if (error.name === 'TokenExpiredError') error = handleJWTExpireError();
-		prodError(error, res);
-	}
+	devError(err, req, res);
+	// if (process.env.NODE_ENV === 'production') {
+	// 	let error = { ...err };
+	// 	if (error.name === 'CastError') error = handleCastErrorDB(error);
+	// 	if (error.code === 11000) error = handleDuplicateFieldsErrorDB(error);
+	// 	if (error.name === 'ValidationError')
+	// 		error = handleValidationErrorDB(error);
+	// 	if (error.name === 'JsonWebTokenError') error = handleJWTError();
+	// 	if (error.name === 'TokenExpiredError') error = handleJWTExpireError();
+	// 	prodError(error, res);
+	// }
 };
 
 export default globalErrorController;
