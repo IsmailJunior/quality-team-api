348741b237e640e2edf34eb95654a00ad2d62302
Ismail Salah <ismailsalah@Ismails-MacBook-Air.local>
2024-09-19 02:37:32 +0300

    Update

--- controllers/errorController.mjs
+++ controllers/errorController.mjs
@@ -1,11 +1,41 @@
+// import AppError from '../utils/appError.mjs';
+
+// const handleCastErrorDB = (err) => {
+// 	const message = `Invalid ${err.path}: ${err.value}.`;
+// 	return new AppError(message, 400);
+// };
+
+// const handleDuplicateFieldsErrorDB = (err) => {
+// 	const value = err.errorResponse.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+// 	const message = `Duplicate Field value: ${value}, Please use another value!`;
+// 	return new AppError(message, 400);
+// };
+
+// const handleValidationErrorDB = (err) => {
+// 	const errors = Object.values(err.errors).map((element) => element.message);
+// 	const message = `Invalid input data, ${errors.join(' ')}`;
+// 	return new AppError(message, 400);
+// };
+
+// const handleJWTError = () =>
+// 	new AppError('Invalid token, Please log in again!', 401);
+
+// const handleJWTExpireError = () =>
+// 	new AppError('Your token has expired!, Please log in again.', 401);
+
 const globalErrorController = (err, req, res, _next) => {
-	// const error = {
-	// 	...err,
-	// };
-	// if (err.name === 'ValidationError') {
-	// 	error.statusCode = 422;
-	// 	error.status = 'ValidationError';
-	// }
+	console.log(err);
+	if (err.name === 'ValidationError') {
+		// eslint-disable-next-line no-param-reassign
+		err.statusCode = 422;
+		// eslint-disable-next-line no-param-reassign
+		err.status = 'ValidationError';
+	} else if (err.code === 11000) {
+		// eslint-disable-next-line no-param-reassign
+		err.statusCode = 400;
+		// eslint-disable-next-line no-param-reassign
+		err.status = 'MongoServerError';
+	}
 
 	res.status(err.statusCode).json({
 		status: err.status,
