9896c726bc5607f4ad51f5a1f59d2fdf78762ae9
System Administrator <root@Ismails-MacBook-Air.local>
2024-10-22 00:05:21 +0300

    final :)

--- app.mjs
+++ app.mjs
@@ -29,22 +29,22 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express();
 
 const limiter = rateLimit({
-	max: 100,
+	max: 500,
 	windowMs: 60 * 60 * 1000,
 	message: 'Too many requests from this IP, Please try again in an hour!',
 });
-// app.use(helmet());
-// app.use(limiter);
+app.use(helmet());
+app.use(limiter);
 
 // Middlewares
 app.set('view engine', 'ejs');
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-// app.use(mongoSanitize());
-// app.use(xss());
-// app.use(hpp());
-// app.use(compression());
+app.use(cors());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(compression());
 if (process.env.NODE_ENV !== 'production') {
 	app.use(logger('dev'));
 }
